bfde796a14387f6b6417d322bb999991ef8475c1
Maksymilian <ts3joker@gmail.com>
2022-07-02 16:25:51 +0200

    Initial Commit

---
+++ FIrst/FIrstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIrstApp.swift
+//  FIrst
+//
+//  Created by Maksymilian Patoń on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FIrstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
